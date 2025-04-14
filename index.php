80d2e3e1939fd7dc2b4312417419ed30c0cfacc2
DanielaBueno <148583342+DanielaBueno@users.noreply.github.com>
2025-04-13 23:16:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,26 +6,28 @@
     <title>Herbario Catatumbo Sarare - Unipamplona</title>
     <meta name="description" content="Bienvenido al Herbario Catatumbo Sarare, un espacio dedicado a la preservación y estudio de la biodiversidad de nuestra región.">
     <meta name="keywords" content="herbario, biodiversidad, Catatumbo, Sarare, conservación de plantas, naturaleza, estudio de flora, universidad de pamplona, unipamplona, ejemplares, taxonomia, biologia, botanica">
-    <link rel="stylesheet" href="css/index.css">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <header>
-        <img src="Media/logo.png" alt="Logo">
+        <img src="media/logo.png" alt="Logo">
         <h2>Herbario Catatumbo Sarare</h2>
     </header>
 
-    <div class="main-content">
+    <main>
         <h1>Bienvenido al Herbario Catatumbo Sarare</h1>
         <p>Un espacio dedicado a la preservación y estudio de la biodiversidad de nuestra región.</p>
-        <div class="login-container">
+        <section>
             <p>Para continuar, por favor inicia sesión</p>
+            <br>
             <a href="login.php">
-            <button class="login-button">Iniciar sesión</button></a>
-        </div>
-    </div>
+            <button>Iniciar sesión</button></a>
+        </section>
+    </main>
 
-    <footer class="footer">
+    <footer>
         <p>&copy; 2025 Herbario Catatumbo Sarare. Todos los derechos reservados.</p>
     </footer>
 </body>
